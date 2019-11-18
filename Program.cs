1ec49700b4a36f06d32338d69d76ae2fd47a416f
FortunatoMugnano <fortunato.mugnano88@gmail.com>
2019-11-18 15:55:40 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare a new List variable
+            List<int> cohorts = new List<int>();
+
+            //Add method
+            cohorts.Add(34);
+            cohorts.Add(35);
+            cohorts.Add(36);
+            cohorts.Add(37);
+
+            //Another way we can initialize this list
+            List<string> classes = new List<string>()
+            {
+                "Cohort 34",
+                "Cohort 35",
+                "Cohort 36",
+                "Cohort 37",
+            };
+
+            //Remove method
+            classes.Remove("Cohort 34");
+            classes.Add("Cohort 38");
+
+            //Looping with ForEach
+            foreach (string nssClass in classes)
+            {
+                Console.WriteLine(nssClass);
+            }
+
+            foreach (int nssClass in cohorts)
+            {
+                Console.WriteLine(nssClass);
+            }
+
+            //Conditional looping
+            foreach (int nssClass in cohorts)
+            {
+                if (nssClass == 35)
+                {
+                    Console.WriteLine($"The Best Cohort: {nssClass}");
+                }
+                else
+                {
+                    Console.WriteLine($"A Perfectly fine Cohort: {nssClass}");
+                }
+            }
+
+        }
+    }
+}
